3ad0a41696bd86688d9338e82b3191156d11ac73
laiscrz <lais.albino2003@gmail.com>
2024-11-14 21:04:42 -0300

    feat: added springboot project

---
+++ src/main/java/com/taligado/app/GreenOnApplication.java
@@ -0,0 +1,13 @@
+package com.taligado.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenOnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taligado/app/GreenOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taligado.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
